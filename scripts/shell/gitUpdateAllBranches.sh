11a1aee0545ebc46ab4ea0870e1addcb12bb0fef
Aline Brito <nbrito.aline@gmail.com>
2017-05-30 20:21:55 -0300

    Correção do script para atualizar todas as branches dos sistemas.

--- scripts/shell/gitUpdateAllBranches.sh
+++ scripts/shell/gitUpdateAllBranches.sh
@@ -10,15 +10,13 @@ do
 	echo ""	
 	echo "###################### PROJECT $count -  $line ######################"
 	cd $PATH_PROJECT"/"$line
-	git clean -df
-	git checkout -- .
-	git fetch --all
 	for branch in `git branch -r | sed -r 's/^.{9}//' | grep -v HEAD` ; do 
-	#for branch in `git branch -r | cut -d '/' -f2 | grep -v HEAD` ; do 
 		echo ""
 		echo "-----"
 		echo "CHECKOUT $branch"
-		git checkout $branch && git pull origin $branch;
+		git checkout $branch
+		git reset --hard "origin/"$branch ## reset modificações na branch.
+		git pull origin $branch;
 	done
 	count=$((count+1));
 done < $1
