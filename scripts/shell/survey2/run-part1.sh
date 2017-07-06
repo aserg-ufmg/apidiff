f2d731dd8cbf3d176a837eb9574b555975b49f2c
Aline Brito <nbrito.aline@gmail.com>
2017-07-06 20:14:50 -0300

    apidiff-67 :: Scripts para o survey II.

---
+++ scripts/shell/survey2/run-part1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#bash gitClone.sh listUrlAndFullNameProjects.csv
+
+bash gitUpdateAllBranches.sh listFullNameProjects-part1.csv
+
+#bash gitStatus.sh listFullNameProjects-part1.csv
---
+++ scripts/shell/survey2/run-part3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#bash gitClone.sh listUrlAndFullNameProjects.csv
+
+bash gitUpdateAllBranches.sh listFullNameProjects-part3.csv
+
+#bash gitStatus.sh listFullNameProjects-part2.csv
