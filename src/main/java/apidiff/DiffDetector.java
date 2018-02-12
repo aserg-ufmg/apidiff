f733732fc54e2ae1022d422411686c0bb7ef7166
Aline Brito <nbrito.aline@gmail.com>
2018-02-12 11:16:05 -0200

    update javadoc

--- src/main/java/apidiff/DiffDetector.java
+++ src/main/java/apidiff/DiffDetector.java
@@ -7,18 +7,63 @@ import apidiff.enums.Classifier;
 
 public interface DiffDetector {
 	
+	/**
+	 * Analyzing changes performed in specific commit.
+	 * @param commitId - SHA key
+	 * @param classifier
+	 * @return Result
+	 * @throws Exception
+	 */
 	public Result detectChangeAtCommit(String commitId, Classifier classifier) throws Exception;;
 	
+	/**
+	 * Analyzing changes performed in whole history.
+	 * @param branch
+	 * @param classifiers
+	 * @return Result
+	 * @throws Exception
+	 */
 	public Result detectChangeAllHistory(String branch, List<Classifier> classifiers) throws Exception;
 	
+	/**
+	 * Analyzing changes performed in whole history.
+	 * @param branch
+	 * @param classifier
+	 * @return
+	 * @throws Exception
+	 */
 	public Result detectChangeAllHistory(String branch, Classifier classifier) throws Exception;
 	
+	/**
+	 * Analyzing changes performed in whole history.
+	 * @param classifiers
+	 * @return
+	 * @throws Exception
+	 */
 	public Result detectChangeAllHistory(List<Classifier> classifiers) throws Exception;
 	
+	/**
+	 * Analyzing changes performed in whole history.
+	 * @param classifier
+	 * @return Result
+	 * @throws Exception
+	 */
 	public Result detectChangeAllHistory(Classifier classifier) throws Exception;
 	
+	/**
+	 * Analyzing changes performed in new commits.
+	 * @param classifiers
+	 * @return Result
+	 * @throws Exception
+	 */
 	public Result fetchAndDetectChange(List<Classifier> classifiers) throws Exception;
 	
+	/**
+	 * Analyzing changes performed in new commits.
+	 * @param classifier
+	 * @return Result
+	 * @throws Exception
+	 */
 	public Result fetchAndDetectChange(Classifier classifier) throws Exception;
 	
 }
