48efd39443206e231e23083f130c9094a357f199
Aline Brito <14023536+alinebrito@users.noreply.github.com>
2021-10-30 21:07:19 -0300

    test: Add tests to Rename Method and Inline Method

--- src/test/java/apidiff/internal/analysis/MethodDiffTest.java
+++ src/test/java/apidiff/internal/analysis/MethodDiffTest.java
@@ -29,38 +29,113 @@ class MethodDiffTest {
 	@Test
 	void testDetectPushDown() {
 		
-		Result result = diff.detectChangeAtCommit("f36ba8de48ecc748a498d87595597edfb7abb0e8", Classifier.API);
+		Result result = diff
+				.detectChangeAtCommit("f36ba8de", 
+				Classifier.API);
 		
 		List<Change> changes = result.getChangeMethod().stream()
-			.filter(change -> Category.METHOD_PUSH_DOWN.equals(change.getCategory()))
-			.filter(change -> "com.github.mikephil.charting.data.Entry".equals(change.getPath()))
-			.filter(change -> "public float[] getVals()".equals(change.getElement()))
+			.filter(change -> Category.METHOD_PUSH_DOWN
+				.equals(change.getCategory()))
+			.filter(change -> "com.github.mikephil.charting.data.Entry"
+				.equals(change.getPath()))
+			.filter(change -> "public float[] getVals()"
+				.equals(change.getElement()))
 			.collect(Collectors.toList());
 		
-		assertEquals(changes.size(), 1, "There is no Push Down Method operation");
-		assertTrue(changes.get(0).isBreakingChange(), "Push Down Method is a breaking change");
-		assertEquals(changes.get(0).getElementType(), ElementType.METHOD, "There is no change at the method level");
-		assertFalse(changes.get(0).isDeprecated(), "There is no deprecated annotation");
-		assertTrue(changes.get(0).containsJavadoc(), "There is JavaDoc annotation");
+		assertEquals(changes.size(), 1, 
+				"There is no Push Down Method operation");
+		assertTrue(changes.get(0).isBreakingChange(), 
+				"Push Down Method is a breaking change");
+		assertEquals(changes.get(0).getElementType(), 
+				ElementType.METHOD, 
+				"There is no change at the method level");
+		assertFalse(changes.get(0).isDeprecated(), 
+				"There is no deprecated annotation");
+		assertTrue(changes.get(0).containsJavadoc(), 
+				"There is JavaDoc annotation");
 		
 	}
 	
 	@Test
 	void testDetectPullUp() {
-		Result result = diff.detectChangeAtCommit("2b886a463e85a0e35e9641b75250b0460b6307c8", Classifier.API);
+		Result result = diff.detectChangeAtCommit("2b886a46", Classifier.API);
 		
 		List<Change> changes = result.getChangeMethod().stream()
-			.filter(change -> Category.METHOD_PULL_UP.equals(change.getCategory()))
-			.filter(change -> "com.github.mikephil.charting.charts.BarLineChartBase".equals(change.getPath()))
-			.filter(change -> "public Highlight getHighlightByTouchPoint(float x, float y)".equals(change.getElement()))
+			.filter(change -> 
+				Category.METHOD_PULL_UP.equals(change.getCategory()))
+			.filter(change -> 
+				"com.github.mikephil.charting.charts.BarLineChartBase"
+				.equals(change.getPath()))
+			.filter(change -> 
+				"public Highlight getHighlightByTouchPoint(float x, float y)"
+				.equals(change.getElement()))
 			.collect(Collectors.toList());
 		
-		assertEquals(changes.size(), 1, "There is no Pull Up Method operation");
-		assertFalse(changes.get(0).isBreakingChange(), "Pull Up Method operation is not breaking change");
-		assertEquals(changes.get(0).getElementType(), ElementType.METHOD, "There is no change at the method level");
-		assertFalse(changes.get(0).isDeprecated(), "There is no deprecated annotation");
-		assertTrue(changes.get(0).containsJavadoc(), "There is JavaDoc annotation");
+		assertEquals(changes.size(), 1, 
+				"There is no Pull Up Method operation");
+		assertFalse(changes.get(0).isBreakingChange(), 
+				"Pull Up Method operation is not breaking change");
+		assertEquals(changes.get(0).getElementType(), 
+				ElementType.METHOD, 
+				"There is no change at the method level");
+		assertFalse(changes.get(0).isDeprecated(), 
+				"There is no deprecated annotation");
+		assertTrue(changes.get(0).containsJavadoc(), 
+				"There is JavaDoc annotation");
 		
+	}
+	
+	@Test
+	void testDetectRename() {
+		Result result = diff.detectChangeAtCommit("4b00e834", Classifier.API);
+
+		List<Change> changes = result.getChangeMethod().stream()
+			.filter(change -> Category.METHOD_RENAME
+					.equals(change.getCategory()))
+			.filter(change -> "com.github.mikephil.charting.highlight.ChartHighlighter"
+				.equals(change.getPath()))
+			.filter(change -> "protected SelectionDetail getDetail(IDataSet set, int dataSetIndex, float xVal, DataSet.Rounding rounding)"
+				.equals(change.getElement()))
+			.collect(Collectors.toList());
+		
+		assertEquals(changes.size(), 1, 
+				"There is no Rename Method operation");
+		assertTrue(changes.get(0).isBreakingChange(), 
+				"Rename Method operation is breaking change");
+		assertEquals(changes.get(0).getElementType(), 
+				ElementType.METHOD, 
+				"There is no change at the method level");
+		assertFalse(changes.get(0).isDeprecated(), 
+				"There is no deprecated annotation");
+		assertTrue(changes.get(0).containsJavadoc(), 
+				"There is JavaDoc annotation");
+		
+	}
+	
+	@Test
+	void testDetectInline() {
+		Result result = diff.detectChangeAtCommit("4ab4c0ba", Classifier.API);
+
+		List<Change> changes = result.getChangeMethod().stream()
+			.filter(change -> Category.METHOD_INLINE
+					.equals(change.getCategory()))
+			.filter(change -> "com.xxmassdeveloper.mpchartexample.BarChartActivity"
+				.equals(change.getPath()))
+			.filter(change -> "protected int getLayout()"
+				.equals(change.getElement()))
+			.collect(Collectors.toList());
+		
+		assertEquals(changes.size(), 1, 
+				"There is no Inline Method operation");
+		assertTrue(changes.get(0).isBreakingChange(), 
+				"Inline Method operation is breaking change");
+		assertEquals(changes.get(0).getElementType(), 
+				ElementType.METHOD, 
+				"There is no change at the method level");
+		assertFalse(changes.get(0).isDeprecated(), 
+				"There is no deprecated annotation");
+		assertFalse(changes.get(0).containsJavadoc(), 
+				"There is no JavaDoc annotation");
 	}
 
 }
